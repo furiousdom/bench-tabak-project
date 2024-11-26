665353279db535ec86f39b061de9368225d1f149
Dominik Tabak <tabakdominik@gmail.com>
2024-11-26 09:57:52 +0100

    Add fetch user endpoint

--- server/user/index.ts
+++ server/user/index.ts
@@ -5,12 +5,34 @@ export const router = new Router();
 
 const users = [
   {
-    name: 'Test'
+    name: 'Mike'
+  },
+  {
+    name: 'Alice'
   }
 ];
 
 router.get('/users', (ctx: Context) => {
   ctx.body = {
     users
+  }
+});
+
+router.get('/users/:id', (ctx: Context) => {
+  const { id } = ctx.params;
+  let user = null;
+
+  try {
+    user = users[parseInt(id)];
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = { error: 'User does not exist' };
+  }
+
+  if (!user) {
+    ctx.status = 404;
+    ctx.body = { error: 'User not found' };
+  } else {
+    ctx.body = { user };
   }
 });
