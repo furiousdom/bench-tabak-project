47e4388fa8c3926424cc3ffb28a329222cb9a330
Dominik Tabak <tabakdominik@gmail.com>
2024-11-28 11:24:28 +0100

    Add database related variables to `Config` type

--- server/config.ts
+++ server/config.ts
@@ -3,17 +3,32 @@ import dotenv from 'dotenv';
 interface ENV {
   NODE_ENV: string | undefined;
   PORT: number | undefined;
+  DB_HOST: string | undefined;
+  DB_PORT: number | undefined;
+  DB_NAME: string | undefined;
+  DB_USER: string | undefined;
+  DB_PASSWORD: string | undefined;
 }
 
 interface Config {
   NODE_ENV: string;
   PORT: number;
+  DB_HOST: string | undefined;
+  DB_PORT: number | undefined;
+  DB_NAME: string | undefined;
+  DB_USER: string | undefined;
+  DB_PASSWORD: string | undefined;
 }
 
 const getConfig = (): ENV => {
   return {
     NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT ? Number(process.env.PORT) : undefined
+    PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
+    DB_HOST: process.env.DATABASE_HOST,
+    DB_PORT: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : undefined,
+    DB_NAME: process.env.DATABASE_NAME,
+    DB_USER: process.env.DATABASE_USER,
+    DB_PASSWORD: process.env.DATABASE_PASSWORD,
   };
 };
 
