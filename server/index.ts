1c486133637574feb38f11c2c50daea20a5ba6c7
Dominik Tabak <tabakdominik@gmail.com>
2024-11-29 13:59:24 +0100

    Move server setup to `app` module & initDB and runApp in `index` module

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,5 @@
-import bodyParser from 'koa-bodyparser';
-import config from './config';
-import Koa from 'koa';
-import { router as userRouter } from './user/index';
+import { initDatabase } from './database/index';
+import runApp from './app';
 
-const { PORT } = config;
-
-const app = new Koa();
-
-app.use(bodyParser());
-app.use(userRouter.routes());
-
-const server = app
-  .listen(PORT, () => console.log(`Running on http://localhost:${PORT}/`))
-  .on('error', err => console.error(err));
-
-export default server;
+initDatabase()
+  .then(() => runApp());
