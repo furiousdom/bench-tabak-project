45a11b4fea926225ad5dfdf23c90daa16557e07a
Dominik Tabak <tabakdominik@gmail.com>
2024-12-10 13:31:21 +0100

    Change all users endpoints to query database instead of array

--- server/user/index.ts
+++ server/user/index.ts
@@ -1,29 +1,27 @@
 import { Context } from 'koa';
 import Router from 'koa-router';
+import { getDatabase } from '../database';
 
 export const router = new Router();
 
-const users = [
-  {
-    name: 'Mike'
-  },
-  {
-    name: 'Alice'
-  }
-];
-
-router.get('/users', (ctx: Context) => {
+router.get('/users', async (ctx: Context) => {
+  const db = await getDatabase();
+  const em = db.em.fork();
+  const users = await em.find('User', {});
   ctx.body = {
     users
   }
 });
 
-router.get('/users/:id', (ctx: Context) => {
+router.get('/users/:id', async (ctx: Context) => {
+  const db = await getDatabase();
+  const em = db.em.fork();
+
   const { id } = ctx.params;
   let user = null;
 
   try {
-    user = users[parseInt(id)];
+    user = await em.findOne('User', { id });
   } catch (err) {
     ctx.status = 400;
     ctx.body = { error: 'User does not exist' };
