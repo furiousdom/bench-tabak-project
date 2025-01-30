10412ac852dab7cd34d6497621aef69eaaecdd2c
Dominik Tabak <tabakdominik@gmail.com>
2025-01-30 15:51:30 +0100

    Beautify code in user router

--- server/user/index.ts
+++ server/user/index.ts
@@ -1,13 +1,13 @@
 import Router from 'koa-router';
 import { UserController } from './user.controller';
 
-export function createUserRouter(ctrl: UserController) {
+function createUserRouter(ctrl: UserController) {
   const router = new Router();
 
-  router
+  return router
     .get('/users', ctrl.list)
     .get('/users/:id', ctrl.get)
     .post('/users', ctrl.create);
-
-  return router;
 }
+
+export { createUserRouter };
