a9e27f20b6cc3a8808a32f514bbef5bd53a50449
Dominik Tabak <tabakdominik@gmail.com>
2025-02-07 09:03:52 +0100

    Move `setupDependencies` invocation to `server/index.ts`

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,10 @@
-import { bottle, setupDependencies } from './infrastructure/framework';
 import bodyParser from 'koa-bodyparser';
 import { createRouter } from './router';
-import { Services as DbServices } from './database';
 import Koa from 'koa';
+import Bottle from 'bottlejs';
 
-function setupApp(dbServices: DbServices): Koa {
-  setupDependencies(dbServices);
-
+function setupApp(bottle: Bottle): Koa {
   const router = createRouter(bottle);
-
   const app = new Koa();
 
   app.use(bodyParser());
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import config from './config';
 import { getDatabase } from './database/index';
 import setupApp from './app';
+import { setupDependencies } from './infrastructure/framework';
 
 const { PORT } = config;
 
 getDatabase()
-  .then(dbServices => setupApp(dbServices))
+  .then(dbServices => setupDependencies(dbServices))
+  .then(bottle => setupApp(bottle))
   .then(app => app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}/`)));
