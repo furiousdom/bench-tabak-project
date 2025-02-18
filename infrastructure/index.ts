3f958b7a15bd33ab15a374a82fb22b9f69919cc9
Dominik Tabak <tabakdominik@gmail.com>
2025-02-18 10:34:24 +0100

    Remove bucket from Pulumi project

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,8 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+// // Create an AWS resource (S3 Bucket)
+// const bucket = new aws.s3.BucketV2("my-bucket");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// // Export the name of the bucket
+// export const bucketName = bucket.id;
