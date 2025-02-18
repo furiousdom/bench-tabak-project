b9c4213b7063f4895722595bee1cc1a01b16ac80
Dominik Tabak <tabakdominik@gmail.com>
2025-02-18 12:49:30 +0100

    Add starting EC2 & RDS instances via Pulumi

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,10 +1,40 @@
-import * as aws from "@pulumi/aws";
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
 
-export const vpc = new aws.ec2.Vpc("pulumi-demo-vpc", {
-  cidrBlock: "10.0.0.0/16",
-  enableDnsSupport: true,
-  enableDnsHostnames: true,
-  tags: {
-    Name: "pulumi-demo-vpc",
-  },
+const config = new pulumi.Config();
+const currentStack = pulumi.getStack();
+
+const dbUsername = config.requireSecret('dbUsername');
+const dbPassword = config.requireSecret('dbPassword');
+
+const securityGroup = new aws.ec2.SecurityGroup(`reels-security-group-${currentStack}`, {
+  ingress: [
+    { protocol: 'tcp', fromPort: 22, toPort: 22, cidrBlocks: ['0.0.0.0/0'] },
+    { protocol: 'tcp', fromPort: 5432, toPort: 5432, cidrBlocks: ['0.0.0.0/0'] }
+  ]
 });
+
+const dbInstance = new aws.rds.Instance(`reels-database-${currentStack}`, {
+    engine: 'postgres',
+    instanceClass: 'db.t4g.micro',
+    allocatedStorage: 20,
+    username: dbUsername,
+    password: dbPassword,
+    publiclyAccessible: true,
+    skipFinalSnapshot: true,
+    vpcSecurityGroupIds: [securityGroup.id],
+});
+
+const ec2Instance = new aws.ec2.Instance(
+  `reels-server-${currentStack}`,
+  {
+    instanceType: 't3.micro',
+    ami: 'ami-07a64b147d3500b6a',
+    securityGroups: [securityGroup.name]
+  },
+  { dependsOn: [dbInstance] }
+);
+
+// Export outputs
+export const ec2PublicIp = ec2Instance.publicIp;
+export const dbEndpoint = dbInstance.endpoint;
